007417695a0a39bf2dfe9042b5ed98e6ab8c8cb6
ahmadzaidan99 <zaidan.ahmad@hotmail.com>
2024-03-25 02:30:15 +0300

    Constant User Check

--- app/(course)/courses/[courseId]/chapters/[chapterId]/_components/video-player.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/_components/video-player.tsx
@@ -1,14 +1,15 @@
-"use client";
+'use client';
 
-import axios from "axios";
-import MuxPlayer from "@mux/mux-player-react";
-import { useState } from "react";
-import { toast } from "react-hot-toast";
-import { useRouter } from "next/navigation";
-import { Loader2, Lock } from "lucide-react";
+import axios from 'axios';
+import MuxPlayer from '@mux/mux-player-react';
+import { useEffect, useState } from 'react';
+import { toast } from 'react-hot-toast';
+import { useRouter } from 'next/navigation';
+import { Loader2, Lock } from 'lucide-react';
 
-import { cn } from "@/lib/utils";
-import { useConfettiStore } from "@/hooks/use-confetti-store";
+import { cn } from '@/lib/utils';
+import { useConfettiStore } from '@/hooks/use-confetti-store';
+import { useUser } from '@clerk/nextjs';
 
 interface VideoPlayerProps {
   playbackId: string;
@@ -18,7 +19,7 @@ interface VideoPlayerProps {
   isLocked: boolean;
   completeOnEnd: boolean;
   title: string;
-};
+}
 
 export const VideoPlayer = ({
   playbackId,
@@ -33,50 +34,64 @@ export const VideoPlayer = ({
   const router = useRouter();
   const confetti = useConfettiStore();
 
+  const { user } = useUser();
+  useEffect(() => {
+    const reloadSession = async () => {
+      try {
+        await user?.reload();
+      } catch {
+        //Logged out
+      }
+    };
+    const intervalId = setInterval(() => {
+      reloadSession();
+    }, 20000);
+    return () => clearInterval(intervalId);
+  });
+
   const onEnd = async () => {
     try {
       if (completeOnEnd) {
-        await axios.put(`/api/courses/${courseId}/chapters/${chapterId}/progress`, {
-          isCompleted: true,
-        });
+        await axios.put(
+          `/api/courses/${courseId}/chapters/${chapterId}/progress`,
+          {
+            isCompleted: true,
+          },
+        );
 
         if (!nextChapterId) {
           confetti.onOpen();
         }
 
-        toast.success("Progress updated");
+        toast.success('Progress updated');
         router.refresh();
 
         if (nextChapterId) {
-          router.push(`/courses/${courseId}/chapters/${nextChapterId}`)
+          router.push(`/courses/${courseId}/chapters/${nextChapterId}`);
         }
       }
     } catch {
-      toast.error("Something went wrong");
+      toast.error('Something went wrong');
     }
-  }
+  };
 
   return (
-    <div className="relative aspect-video">
+    <div className='relative aspect-video'>
       {!isReady && !isLocked && (
-        <div className="absolute inset-0 flex items-center justify-center bg-slate-800">
-          <Loader2 className="h-8 w-8 animate-spin text-secondary" />
+        <div className='absolute inset-0 flex items-center justify-center bg-slate-800'>
+          <Loader2 className='h-8 w-8 animate-spin text-secondary' />
         </div>
       )}
       {isLocked && (
-        <div className="absolute inset-0 flex items-center justify-center bg-slate-800 flex-col gap-y-2 text-secondary">
-          <Lock className="h-8 w-8" />
-          <p className="text-sm">
-            This chapter is locked
-          </p>
+        <div className='absolute inset-0 flex items-center justify-center bg-slate-800 flex-col gap-y-2 text-secondary'>
+          <Lock className='h-8 w-8' />
+          <p className='text-sm'>This chapter is locked</p>
         </div>
       )}
       {!isLocked && (
         <MuxPlayer
           title={title}
-          className={cn(
-            !isReady && "hidden"
-          )}
+          className={cn(!isReady && 'hidden')}
           onCanPlay={() => setIsReady(true)}
           onEnded={onEnd}
           autoPlay
@@ -84,5 +99,5 @@ export const VideoPlayer = ({
         />
       )}
     </div>
-  )
-}
+  );
+};
--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,24 +1,36 @@
-import { Navbar } from "./_components/navbar";
-import { Sidebar } from "./_components/sidebar";
+'use client';
+import { redirect } from 'next/navigation';
+import { Navbar } from './_components/navbar';
+import { Sidebar } from './_components/sidebar';
+import { auth, clerkClient, useAuth, useSession, useUser } from '@clerk/nextjs';
+import { useEffect } from 'react';
 
-const DashboardLayout = ({
-  children
-}: {
-  children: React.ReactNode;
-}) => {
-  return ( 
-    <div className="h-full">
-      <div className="h-[80px] md:pl-56 fixed inset-y-0 w-full z-50">
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  const { user } = useUser();
+  useEffect(() => {
+    const reloadSession = async () => {
+      try {
+        await user?.reload();
+      } catch {
+        //Logged out
+      }
+    };
+    const intervalId = setInterval(() => {
+      reloadSession();
+    }, 20000);
+    return () => clearInterval(intervalId);
+  });
+  return (
+    <div className='h-full'>
+      <div className='h-[80px] md:pl-56 fixed inset-y-0 w-full z-50'>
         <Navbar />
       </div>
-      <div className="hidden md:flex h-full w-56 flex-col fixed inset-y-0 z-50">
+      <div className='hidden md:flex h-full w-56 flex-col fixed inset-y-0 z-50'>
         <Sidebar />
       </div>
-      <main className="md:pl-56 pt-[80px] h-full">
-        {children}
-      </main>
+      <main className='md:pl-56 pt-[80px] h-full'>{children}</main>
     </div>
-   );
-}
- 
+  );
+};
+
 export default DashboardLayout;
